bfdb00631d10637f8d907c949ae4bd01df2e8655
ismail tafida <tafidashukra@gmail.com>
2023-10-01 23:26:39 -0700

    Initial commit

---
+++ Entry_form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Card system</title>
+</head>
+<body>
+    <section>
+        <div>
+            <form method="post">
+                <label>
+                    Full Name: <input type="text">
+                </label>
+                <label>
+                    phone number: <input type="text">
+                </label>
+                <label>
+                    Email address: <input type="text">
+                </label>
+                <label>
+                    house address: <input type="text">
+                </label>
+                <label>
+                    nin number: <input type="text">
+                </label>
+                
+            </form>
+        </div>
+    </section>
+</body>
+</html>
